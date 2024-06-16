48d287d523ecb0a5076f556ed8485b18ffde4ad1
hyunzinbak <hgggny@gmail.com>
2024-06-16 18:51:12 +0900

    ✨ spring-core : section 2 - 스프링 핵심 원리 이해 1

---
+++ core/src/main/java/hyunzinbak/core/OrderApp.java
@@ -0,0 +1,24 @@
+package hyunzinbak.core;
+
+import hyunzinbak.core.member.Grade;
+import hyunzinbak.core.member.Member;
+import hyunzinbak.core.member.MemberService;
+import hyunzinbak.core.member.MemberServiceImpl;
+import hyunzinbak.core.order.Order;
+import hyunzinbak.core.order.OrderService;
+import hyunzinbak.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
---
+++ core/src/test/java/hyunzinbak/core/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package hyunzinbak.core.order;
+
+import hyunzinbak.core.member.Grade;
+import hyunzinbak.core.member.Member;
+import hyunzinbak.core.member.MemberService;
+import hyunzinbak.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
